18cfd58f8221a36f3fe6f1161403f67761144984
BorisGolub <borisgolub10@gmail.com>
2023-05-26 12:29:11 -0700

    Add Project02 files

---
+++ Task7Test.java
@@ -0,0 +1,14 @@
+package Project02;
+
+public class Task7Test {
+        public static void main(String[] args) {
+
+            Task7 studentA = new A(80, 90, 70);
+            double percentageA = studentA.getPercentage();
+            System.out.println("Student A Percentage: " + percentageA + "%");
+
+            Task7 studentB = new B(75, 85, 80, 90);
+            double percentageB = studentB.getPercentage();
+            System.out.println("Student B Percentage: " + percentageB + "%");
+        }
+}
